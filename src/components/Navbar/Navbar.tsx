4137abb2a0d84ec3b8f05503eec0258d8641a521
Hannes_Aaltonen <hannes.aaltonen@tuni.fi>
2020-03-20 18:44:05 +0100

    deploybuild

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { Store } from '../../store/Store'
+import styled from 'styled-components'
 
 const Navbar: React.FC = () => {
   const history = useHistory()
@@ -15,69 +16,70 @@ const Navbar: React.FC = () => {
 
   return (
     <nav>
-    {
-      !state.toggled 
-      ?
-        <FontAwesomeIcon style={navOpenButtonStyle} onClick={() => dispatch({type: 'TOGGLE'})} size="2x" icon={faBars}/>  
-      : 
-        <div style={navContentStyle}>
-          <FontAwesomeIcon style={navCloseButtonStyle} onClick={() => dispatch({type: 'TOGGLE'})} size="2x" icon={faTimes}/>
-          <ul style={listStyle}>
-            <li style={linkStyle} key={1} onClick={() => handleClick('/')}>Home</li>
-            <li style={linkStyle} key={2} onClick={() => handleClick('/cv')}>CV</li>
-            <li style={linkStyle} key={3} onClick={() => handleClick('/portfolio')}>Portfolio</li>
-            <li style={linkStyle} key={4} onClick={() => handleClick('/contact')}>Contact</li>
-          </ul>
-        </div>        
-    }
+      {
+        !state.toggled 
+        ? <OpenButton>
+            <FontAwesomeIcon onClick={() => dispatch({type: 'TOGGLE'})} size="2x" icon={faBars}/>
+          </OpenButton>
+        : 
+          <Layer>
+            <CloseButton>
+              <FontAwesomeIcon onClick={() => dispatch({type: 'TOGGLE'})} size="2x" icon={faTimes}/>
+            </CloseButton>
+            <List>
+              <Link onClick={() => handleClick('/')}>Home</Link>
+              <Link onClick={() => handleClick('/cv')}>CV</Link>
+              <Link onClick={() => handleClick('/portfolio')}>Portfolio</Link>
+              <Link onClick={() => handleClick('/contact')}>Contact</Link>
+            </List>
+          </Layer>        
+      }
     </nav>
   )
 }
 
-const navOpenButtonStyle: React.CSSProperties = {
-  cursor: 'pointer',
-  margin: '5vh',
-  position: 'fixed',
-  textAlign: 'right',
-  flexFlow: 'row-reverse',
-  float: 'right',
-  right: '0px'  
-}
+const Link = styled.li`
+  color: #f1f1f1;
+  margin: 4vh;
+  cursor: pointer;
+  width: 10%;
+`
 
-const navCloseButtonStyle: React.CSSProperties = {
-  position: 'fixed',
-  cursor: 'pointer',
-  margin: '5vh',
-  color: 'white',
-  right: '0px'
-}
+const List = styled.ul`
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  margin-top: 10vh;
+  padding: 0;
+  font-size: 2.5em;
+  align-items: center;
+`
+const CloseButton = styled.div`
+  position: fixed;
+  cursor: pointer;
+  margin: 7vh;
+  color: white;
+  right: 0;
+`
 
-const navContentStyle: React.CSSProperties = {
-  overflow: 'hidden',
-  backgroundColor: 'black',
-  height: '100%',
-  margin: 0,
-  padding: '0',
-  width: '100%',
-  position: 'fixed',
-  left: 0,
-  top: 0
-}
+const OpenButton = styled.div`
+  position: fixed;
+  cursor: pointer;
+  margin: 7vh;
+  right: 0;
+`
 
-const listStyle: React.CSSProperties = {
-  listStyleType: 'none',
-  display: 'flex',
-  flexDirection: 'column',
-  textAlign: 'center',
-  marginTop: '5vh',
-  padding: '0',
-  fontSize: '2.5em'
-}
-
-const linkStyle: React.CSSProperties = {
-  color: 'white',
-  margin: '4vh',
-  cursor: 'pointer',
-}
+const Layer = styled.div`
+  overflow: hidden;
+  background-color: black;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+`
 
 export default Navbar

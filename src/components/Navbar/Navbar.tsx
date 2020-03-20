c2777cb5865dab8d8759d288274cfeecdd460fa3
Hannes_Aaltonen <hannes.aaltonen@tuni.fi>
2020-03-20 13:16:14 +0100

    styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,13 +5,33 @@ const Navbar: React.FC = () => {
   const history = useHistory()
 
   return (
-    <nav>
-      <li onClick={() => history.push('/')}>Home</li>
-      <li onClick={() => history.push('/cv')}>CV</li>
-      <li onClick={() => history.push('/portfolio')}>Portfolio</li>
-      <li onClick={() => history.push('/contact')}>Contact</li>
+    <nav style={navStyle}>
+      <ul style={listStyle}>
+        <li style={linkStyle} key={1} onClick={() => history.push('/')}>Home</li>
+        <li style={linkStyle} key={2} onClick={() => history.push('/cv')}>CV</li>
+        <li style={linkStyle} key={3} onClick={() => history.push('/portfolio')}>Portfolio</li>
+        <li style={linkStyle} key={4} onClick={() => history.push('/contact')}>Contact</li>
+      </ul>
     </nav>
   )
+}
+
+const navStyle: React.CSSProperties = {
+  width: '100vh',
+  margin: '3vh'
+}
+
+const listStyle: React.CSSProperties = {
+  margin: 0,
+  width: '100vh',
+  listStyleType: 'none',
+  display: 'flex',
+  flexDirection: 'row'
+}
+
+const linkStyle: React.CSSProperties = {
+  color: 'red',
+  marginRight: '5vh'
 }
 
 export default Navbar

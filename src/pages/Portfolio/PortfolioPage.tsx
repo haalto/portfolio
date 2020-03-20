e15e529d781de9fbe0382427866a5bedf1c158ee
Hannes_Aaltonen <hannes.aaltonen@tuni.fi>
2020-03-20 19:49:01 +0100

    deploybuild

--- src/pages/Portfolio/PortfolioPage.tsx
+++ src/pages/Portfolio/PortfolioPage.tsx
@@ -8,12 +8,6 @@ const PortfolioPage: React.FC = () => {
       <Navbar/>
       <h2>On this page I have listed my hobby projects</h2>
       <Project 
-        header="Redgreenblue" 
-        description="Simple web application where you have to guess correct RGB value from three choices"
-        techs={['ReactJS']} 
-        liveUrl="http://redgreenblue.herokuapp.com/" 
-        sourceUrl="https://github.com/haalto/redgreenblue"/>
-      <Project 
         header="ChatApp" 
         description="Full stack real time chat application with web sockets and user authentication"
         techs={['ReactJS, NodeJS', 'MongoDB Atlas, Socket.io']} 
@@ -26,8 +20,14 @@ const PortfolioPage: React.FC = () => {
         liveUrl="" 
         sourceUrl="https://github.com/haalto/twitter-clone"/>
       <Project 
+        header="redgreenblue" 
+        description="Simple web application where you have to guess correct RGB value from three choices"
+        techs={['ReactJS']} 
+        liveUrl="http://redgreenblue.herokuapp.com/" 
+        sourceUrl="https://github.com/haalto/redgreenblue"/>
+      <Project 
         header="PERN-template" 
-        description="Template for developing full stack application with Postgres, Express, ReactJS, NodeJS and TypeScript. Just add .env-file and run docker-compose up and you are ready to start developing!"
+        description="Template for developing full stack web applications with Postgres, Express, ReactJS, NodeJS and TypeScript. Just add .env-file and run docker-compose up and you are ready to start developing!"
         techs={['ReactJS, NodeJS', 'Postgres, TypeScipt', 'Docker']} 
         liveUrl="" 
         sourceUrl="https://github.com/haalto/PERN-template"/>

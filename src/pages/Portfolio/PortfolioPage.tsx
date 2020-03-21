7995a140dd91c36d5422d6387ac96d965780f38c
Hannes_Aaltonen <hannes.aaltonen@tuni.fi>
2020-03-21 14:32:26 +0100

    deploybuild

--- src/pages/Portfolio/PortfolioPage.tsx
+++ src/pages/Portfolio/PortfolioPage.tsx
@@ -51,7 +51,9 @@ const Header = styled.h3`
 `
 
 const Container = styled.div`
-  position: absolute;
+  display: flex;
+  flex-direction: column;
+  position: relative;
   background: #56ab2f;  /* fallback for old browsers */
   background: -webkit-linear-gradient(to right, #a8e063, #56ab2f);  /* Chrome 10-25, Safari 5.1-6 */
   background: linear-gradient(to right, #a8e063, #56ab2f); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */   
